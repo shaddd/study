46d73ed992b3ca5e00c5d5124043ede222573563
shaddd <33366208+shaddd@users.noreply.github.com>
2022-11-15 14:19:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+﻿var mainListDiv = document.getElementById("mainListDiv");
+var mediaButton = document.getElementById("mediaButton");
 
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
